4fde9a8d2d173d70863eb2ebe07d79fed831b14e
Backend-bunny <173007677+backend-bunny@users.noreply.github.com>
2025-03-01 08:51:12 +0000

    Remove terraform references

--- nix/k3s-worker.nix
+++ nix/k3s-worker.nix
@@ -1,14 +1,11 @@
 {
   lib,
   pkgs,
-#  terraform,
   ...
 }: {
   imports = [
     ./hardware-configuration.nix
-#    ./common.nix
   ];
-#  networking.hostName = terraform.hostname;
 
   environment.systemPackages = map lib.lowPrio [
     pkgs.busybox
